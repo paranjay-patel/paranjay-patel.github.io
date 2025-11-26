227aab409e9ee10291e2c6299c11d165ffb6b68e
paranjay-patel <pearlpatel1234@gmail.com>
2025-11-26 12:11:14 +0530

    updated website path in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://paranjay-patel.github.io',
+  base: '/',
   integrations: [tailwind()]
 });
